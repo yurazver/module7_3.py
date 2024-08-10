ff0ed1ecc334983d4a95eb51bf0b378bea8e508e
yurazver <yurazver0@gmail.com>
2024-08-10 06:59:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,29 +8,32 @@ class WordsFinder:
     def get_all_words(self):
         all_words = {}
 
-        for file in self.file_names:
+        for file_name in self.file_names:
             with open(file_name, 'r', encoding='utf-8') as f:
                 words_list = []
                 for line in f:
-                    line = line.translate(str.maketrans('', '', string.punctuation.replace('-', '')))
+                    line = line.translate(str.maketrans('', '', string.punctuation.replace('-', ''))).lower()
                     words_list.extend(line.split())
                     all_words[file_name] = words_list
         return all_words
 
     def find(self, word):
-        all_words = self.get_all_words()
+        word = word.lower()
         result = {}
-        for file_name, words in all_words.items():
-            if word in words:
-                result[file_name] = words.index(word)
-        return result
+        for file_name, words in self.get_all_words().items():
+            for i in range(len(words)):
+                if word == words[i]:
+                    result[file_name] = words.index(word) + 1
+            return result
 
     def count(self, word):
-        all_words = self.get_all_words()
+        word = word.lower()
         result = {}
-        for file_name, words in all_words.items():
+        for file_name, words in self.get_all_words().items():
             result[file_name] = words.count(word)
         return result
+
+    
 
 file_name = 'test_file.txt'
 finder2 = WordsFinder('test_file.txt')
